43e0abf3452ce2e129c7b73bdeb4084cca1b141a
Marr <marat.salah1313@gmail.com>
2023-11-13 07:51:03 +0100

    plug RTK

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+import store from './slices/store.js'
+
 import './index.css'
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
