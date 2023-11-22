902ed36cee44f55bf170c8c86657c79aceb2d75e
Marr <marat.salah1313@gmail.com>
2023-11-22 19:27:45 +0100

    change layout

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,26 +1,6 @@
 import { useSelector } from "react-redux";
 import Editor from "@toast-ui/editor";
 import { useRef, useEffect } from "react";
-/* import showdown from "showdown";
-
-const Example = () => {
-  const currentLevel = useSelector((state) => state.levels.value.currentLevel);
-  const markdown = useSelector((state) => state.levels.value.entities[currentLevel].exercise);
-
-  // use converter for convert md to html automatically
-
-  const converter = new showdown.Converter();
-  converter.setFlavor('github');
-
-  const html = converter.makeHtml(markdown);
-  const innerHtml = { __html: html };
-
-  return (
-    <div className="w-1/2 min-h-full border-2 border-inherit">
-      <span dangerouslySetInnerHTML={innerHtml}></span>
-    </div>
-  );
-}; */
 
 const Example = () => {
   const currentLevel = useSelector((state) => state.levels.value.currentLevel);
@@ -40,8 +20,10 @@ const Example = () => {
   })
 
   return (
-    <div ref={markExampleRef} className="w-1/2 min-h-full border-2 border-inherit">
-    </div>
+    <>
+      <div ref={markExampleRef} className="mt-9 w-1/2 min-h-full border-2 border-inherit">
+      </div>
+    </>
   );
 }
 
