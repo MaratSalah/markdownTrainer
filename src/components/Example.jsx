14ae86a93b28b20d53b93acb53d60e17e7ed03ad
Marr <marat.salah1313@gmail.com>
2024-02-12 20:16:25 +0100

    change colors to light theme

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,30 +1,33 @@
-import { useSelector } from "react-redux";
-import Editor from "@toast-ui/editor";
-import { useRef, useEffect } from "react";
+import { useSelector } from 'react-redux';
+import Editor from '@toast-ui/editor';
+import { useRef, useEffect } from 'react';
 
 const Example = () => {
   const currentLevel = useSelector((state) => state.levels.value.currentLevel);
-  const markdown = useSelector((state) => state.levels.value.entities[currentLevel].exercise);
+  const markdown = useSelector(
+    (state) => state.levels.value.entities[currentLevel].exercise
+  );
 
   const markExampleRef = useRef(null);
 
   // use converter for convert md to html automatically
 
   useEffect(() => {
-    const viewer = new Editor.factory({
+    new Editor.factory({
       el: markExampleRef.current,
       initialValue: markdown,
       height: '500px',
       viewer: true,
     });
-  })
+  });
 
   return (
     <>
-      <div ref={markExampleRef} className="mt-9 w-1/2 min-h-full border-2 border-inherit">
-      </div>
+      <div
+        ref={markExampleRef}
+        className="mt-9 w-1/2 min-h-full border-2 border-inherit"></div>
     </>
   );
-}
+};
 
 export default Example;
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,13 @@
-import cn from "classnames";
-import { useId } from "react";
-import { useSelector } from "react-redux";
+import cn from 'classnames';
+import { useId } from 'react';
+import { useSelector } from 'react-redux';
 
 const ProgressBar = () => {
   const ids = useSelector((state) => state.levels.value.ids);
   const levels = useSelector((state) => state.levels.value.entities);
+
+  const win = 'bg-white';
+  const lose = 'bg-green-500';
 
   const preClasses = {
     'mx-auto': true,
@@ -12,29 +15,31 @@ const ProgressBar = () => {
     'h-10': true,
     'm-2': true,
     'rounded-full': true,
-    'bg-blue-500': true,
-    'bg-red-500': true,
+    [win]: true,
+    [lose]: true,
   };
 
   const classes = ids.map((item) => {
     if (levels[item].ended === 'false') {
-      preClasses['bg-red-500'] = true;
-      preClasses['bg-blue-500'] = false;
+      preClasses[win] = true;
+      preClasses[lose] = false;
       const result = cn(preClasses);
       return result;
     }
 
-    preClasses['bg-red-500'] = false;
-    preClasses['bg-blue-500'] = true;
+    preClasses[win] = false;
+    preClasses[lose] = true;
     const result = cn(preClasses);
     return result;
   });
 
   return (
     <div className="container mx-auto grid grid-cols-5">
-      {classes.map((item, i) => (<button key={useId} className={item}>
-        <span className="object-center">{i + 1}</span>
-      </button>))}
+      {classes.map((item, i) => (
+        <button key={useId} className={item}>
+          <span className="object-center">{i + 1}</span>
+        </button>
+      ))}
     </div>
   );
 };
