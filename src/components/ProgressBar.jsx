42173d109f138a49267bda42ba2fad22f9d72d05
Marr <marat.salah1313@gmail.com>
2024-02-17 16:34:15 +0100

    change buttons of levels

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -6,35 +6,62 @@ const ProgressBar = () => {
   const ids = useSelector((state) => state.levels.value.ids);
   const levels = useSelector((state) => state.levels.value.entities);
 
-  const win = 'bg-white';
-  const lose = 'bg-green-500';
+  const winColor = 'bg-green-400';
+  const winColorHover = 'active:bg-green-700';
+  const winColorActive = 'hover:bg-green-500';
 
-  const preClasses = {
-    'mx-auto': true,
-    'w-10': true,
-    'h-10': true,
-    'm-2': true,
-    'rounded-full': true,
-    [win]: true,
-    [lose]: true,
-  };
+  const loseColor = 'bg-white';
+  const loseColorHover = 'active:bg-gray-700';
+  const loseColorActive = 'hover:bg-gray-400';
+
+  // start classes, which change when player win level
+
+  const preClasses = [
+    {
+      [winColor]: false,
+      [winColorHover]: false,
+      [winColorActive]: false,
+      [loseColor]: true,
+      [loseColorHover]: true,
+      [loseColorActive]: true,
+    },
+    'mx-auto',
+    'my-2',
+    'h-10',
+    'border-2',
+    'rounded-lg',
+    'border-slate-300',
+    'w-48',
+    'cursor-pointer',
+    'transition',
+    'duration-300',
+    'easy-in-out',
+  ];
 
   const classes = ids.map((item) => {
     if (levels[item].ended === 'false') {
-      preClasses[win] = true;
-      preClasses[lose] = false;
+      preClasses[0][winColor] = false;
+      preClasses[0][winColorHover] = false;
+      preClasses[0][winColorActive] = false;
+      preClasses[0][loseColor] = true;
+      preClasses[0][loseColorHover] = true;
+      preClasses[0][loseColorActive] = true;
       const result = cn(preClasses);
       return result;
     }
 
-    preClasses[win] = false;
-    preClasses[lose] = true;
+    preClasses[0][winColor] = true;
+    preClasses[0][winColorHover] = true;
+    preClasses[0][winColorActive] = true;
+    preClasses[0][loseColor] = false;
+    preClasses[0][loseColorHover] = false;
+    preClasses[0][loseColorActive] = false;
     const result = cn(preClasses);
     return result;
   });
 
   return (
-    <div className="container mx-auto grid grid-cols-5">
+    <div className="container mx-4 gap-56 grid grid-cols-5 justify-between">
       {classes.map((item, i) => (
         <button key={useId} className={item}>
           <span className="object-center">{i + 1}</span>
