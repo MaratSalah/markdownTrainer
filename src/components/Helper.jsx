2946f03e89b1e1b51733b5a7024d810a29fa2456
Marr <marat.salah1313@gmail.com>
2024-02-19 20:33:10 +0100

    add helper with styles

--- src/components/Helper.jsx
+++ src/components/Helper.jsx
@@ -1,7 +1,34 @@
+import { useSelector } from 'react-redux';
+import cn from 'classnames';
+
 const Helper = () => {
-  return (
-    <div className="bg-violet-400 rounded-lg p-2 text-white">{'Helper'}</div>
+  const helperIsShow = useSelector((state) => state.helper.value);
+
+  const currentLevel = useSelector((state) => state.levels.value.currentLevel);
+  const rightAnswer = useSelector(
+    (state) => state.levels.value.entities[currentLevel].exercise
   );
+
+  const opacity = 'opacity-0';
+
+  const helperClasses = [
+    { [opacity]: true },
+    'transition',
+    'bg-violet-400',
+    'rounded-lg',
+    'p-2',
+    'text-white',
+    'duration-300',
+    'ease-in',
+  ];
+
+  if (helperIsShow === false) {
+    helperClasses[0][opacity] = true;
+  } else {
+    helperClasses[0][opacity] = false;
+  }
+
+  return <div className={cn(helperClasses)}>{rightAnswer}</div>;
 };
 
 export default Helper;
