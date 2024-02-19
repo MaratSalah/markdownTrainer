298b991be3071bb5ca3e0e92a01291bb8f20c40d
Marr <marat.salah1313@gmail.com>
2024-02-19 17:28:48 +0100

    change store redux

--- src/slices/store.js
+++ src/slices/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userTextReducer from "./usersTextSlice";
-import levelsSliceReducer from "./levelsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import userTextReducer from './usersTextSlice';
+import levelsSliceReducer from './levelsSlice';
+import helperSliceReducer from './helperSlice';
 
 export default configureStore({
   reducer: {
     usersText: userTextReducer,
     levels: levelsSliceReducer,
+    helper: helperSliceReducer,
   },
 });
