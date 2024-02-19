f708e0ab6fc27ff06516f978b3a7e924d818f518
Marr <marat.salah1313@gmail.com>
2024-02-19 17:29:01 +0100

    add logic for button

--- src/components/HelpButton.jsx
+++ src/components/HelpButton.jsx
@@ -1,5 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeHelperVision } from '../slices/helperSlice';
+
 const HelpButton = () => {
-  return <button>{'?'}</button>;
+  const dispatch = useDispatch();
+  const helperState = useSelector((state) => state.helper.value);
+
+  const onClickButton = () => {
+    if (helperState === false) {
+      dispatch(changeHelperVision(true));
+      return;
+    }
+    dispatch(changeHelperVision(false));
+    return;
+  };
+
+  return <button onClick={onClickButton}>{'?'}</button>;
 };
 
 export default HelpButton;
